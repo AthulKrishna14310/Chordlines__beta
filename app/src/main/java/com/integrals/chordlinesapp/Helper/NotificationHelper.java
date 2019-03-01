ddfc77ef451658d8a22ca99e7a607d26c584a078
AthulKrishna14310 <athul.krishna14310@gmail.com>
2019-03-01 18:24:05 +0530

    Initial beta commit
    Signed-off-by: AthulKrishna14310 <athul.krishna14310@gmail.com>

---
+++ app/src/main/java/com/integrals/chordlinesapp/Helper/NotificationHelper.java
@@ -0,0 +1,72 @@
+package com.integrals.chordlinesapp.Helper;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import com.integrals.chordlinesapp.R;
+
+public class NotificationHelper extends ContextWrapper {
+    private NotificationManager notificationManager;
+
+
+    public NotificationHelper(Context base) {
+        super(base);
+        createNotificationChannels();
+
+    }
+
+
+
+        private void createNotificationChannels() {
+            NotificationChannel notificationChannel = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel = new NotificationChannel("ID_503", "Chordlines ", NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.enableLights(true);
+                notificationChannel.enableLights(true);
+                notificationChannel.setSound(null, null);
+                notificationChannel.setLightColor(Color.WHITE);
+                notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+                getNotificationManager().createNotificationChannel(notificationChannel);
+//mkmkm   mk
+            }
+
+
+        }
+
+
+    public NotificationManager getNotificationManager() {
+        if(notificationManager==null)
+            notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        return notificationManager;
+    }
+
+    public Notification.Builder buildChordlineNotification(int i, String s,
+                                                           Intent intent,
+                                                           PendingIntent pendingIntent) {
+        Notification.Builder notificationbuilder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationbuilder = (Notification.Builder)
+                    new Notification.Builder(getApplicationContext(),"ID_504")
+                    .setContentTitle("Chordlines")
+                    .setContentText(s)
+                    .setWhen(System.currentTimeMillis())
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.logo_chordlines__)
+                    .setProgress(100, 0,true);
+        }
+
+        return notificationbuilder;
+
+
+
+
+
+        }
+}
